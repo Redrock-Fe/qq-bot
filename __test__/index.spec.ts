0963e26626fa19c467416c0b89332968742e0aee
chovrio <1480733667@qq.com>
2022-10-24 16:07:12 +0800

    test: delete test

--- __test__/index.spec.ts
+++
@@ -1,7 +0,0 @@
-import { test } from "vitest";
-import { createBot } from "../packages/core/dist";
-import { Greet } from "../packages/plugins";
-test("aaa", () => {
-  const bot = createBot(2153389851, "yqyq123456", [837470119]);
-  bot.use(Greet);
-});
